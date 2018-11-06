4ec077d4c43d0fa3156f53a16e7fafcc258216ac
Morgan Crouch <morganloel@gmail.com>
2018-11-06 11:59:38 -0600

    created seperate modules

--- bash.js
+++ bash.js
@@ -1,18 +1,10 @@
-// process.stdout.write("prompt > ");
+const pwd = require('./pwd')
+  process.stdout.write("prompt > ");
 
-// process.stdin.on("data", data => {
-//   const cmd = data.toString().trim();
+  process.stdin.on("data", data => {
+   const cmd = data.toString().trim();
 
-//   process.stdout.write("You typed: " + cmd);
-//   process.stdout.write("\nprompt > ");
-// });
+   process.stdout.write("You typed: " + cmd);
+   process.stdout.write("\nprompt > ");
+ });
 
-process.stdout.write("prompt > ");
-
-process.stdin.on("data", data => {
-  const cmd = data.toString().trim();
-  if (cmd === "pwd") {
-    process.stdout.write(`Current directory: ${process.cwd()}`);
-    process.stdout.write("\nprompt > ");
-  }
-});
